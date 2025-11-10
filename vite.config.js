6b7a204f92603e145bd3f513c51d39d427e20587
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-10 19:27:34 +0100

    Fix: supprimer 'base' config pour Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://oumaimaaitsaid.github.io/Oumaima-AitSaid-Portfolio/',
   assetsInclude: ['**/*.pdf'],
   build: {
     rollupOptions: {
